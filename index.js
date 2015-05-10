4c5e46695338df3b09a6f8a7b058229c61a2219c
zchar <zchar@mycolorway.com>
2015-05-10 20:15:26 +0800

    ADD: Methods Inheritance

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-module.exports = function(context) {
+module.exports = function(context, superClass) {
   var _initializer = context.initialize ? context.initialize : (function() {});
+
+  if (superClass) {
+    var so = new superClass();
+    _initializer.prototype = so;
+    so.constructor = _initializer;
+  }
 
   for(var key in context) {
     if ('initialize' != key) { _initializer.prototype[key] = context[key]; }
